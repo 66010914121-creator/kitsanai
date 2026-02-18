7a2e49e53931c9199453d08f769ca3752eabc021
kitsanai <66010914121@msu.ac.th>
2026-02-18 09:47:32 +0700

    1

--- i/connectdb.php
+++ i/connectdb.php
@@ -2,7 +2,7 @@
 <?php
     $host = "localhost";
     $user = "root";
-    $pwd = "";
+    $pwd = "12345gg";
     $db = "4121db";
     $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้ กรุณาลองใหม่");
     mysqli_query($conn,"SET NAMES utf8");
