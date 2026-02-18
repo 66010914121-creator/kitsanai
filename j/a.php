ce0bc96c1a9b36ac1af697c9006ead1387195662
kitsanai <66010914212@msu.ac.th>
2026-02-18 13:55:58 +0700

    1

---
+++ j/a.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>กฤษนัย สรพิมพ์</title>
+</head>
+<body>
+
+    <h1>66010914121 กฤษนัย สรพิมพ์</h1>
+
+    <img src="458172998_1184365656041176_2413883366411555701_n.jpg" alt="รูปโปรไฟล์ของกฤษนัย" width="300">
+
+</body>
+</html>
